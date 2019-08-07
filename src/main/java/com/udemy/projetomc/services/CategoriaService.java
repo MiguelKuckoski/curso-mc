c234e9b65ba62e6c4ce793b4ad6e6a0b15213c55
Miguel Kuckoski <miguelfreitas32@gmail.com>
2019-08-06 23:31:10 -0300

    Alterado endpoint /categorias/{id}

--- src/main/java/com/udemy/projetomc/services/CategoriaService.java
+++ src/main/java/com/udemy/projetomc/services/CategoriaService.java
@@ -4,19 +4,20 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.udemy.projetomc.services.exceptions.ObjectNotFoundException;
 import com.udemy.projetomc.domain.Categoria;
 import com.udemy.projetomc.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	protected CategoriaRepository categoriaRepository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		
-		return categoria.orElse(null);
+
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
