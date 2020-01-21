38f4ffb427336cdd847eea590aef2e98bc650948
miguelkuckoski <miguelfreitas32@gmail.com>
2020-01-20 22:53:11 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/udemy/projetomc/services/CategoriaService.java
+++ src/main/java/com/udemy/projetomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import com.udemy.projetomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,5 +40,9 @@ public class CategoriaService {
 		}catch (org.springframework.dao.DataIntegrityViolationException e) {
 			throw new DataIntegrityViolationException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
 	}
 }
